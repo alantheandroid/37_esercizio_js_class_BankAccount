11b1e9a630818c875146d6661961dd45d2d4dd65
alantheandroid <alessio.ilardi@gmail.com>
2022-06-09 17:08:51 +0200

    feat: add BankAccount class with methods

--- exercise-4.js
+++ exercise-4.js
@@ -1,3 +1,23 @@
+class BankAccount {
+  constructor(funds) {
+    this.funds = funds;
+    console.log(`Starting funds: ${this.funds}`);
+  }
+  deposit(x) {
+    let result = this.funds + x;
+    console.log(`Adding ${x} to ${this.funds} = ${result}`);
+    return (this.funds = result);
+  }
+  withdraw(x) {
+    let result = this.funds - x;
+    console.log(`Subtracting ${x} to ${this.funds} = ${result}`);
+    return (this.funds = result);
+  }
+  view() {
+    console.log(`Current balance: ${this.funds}`);
+  }
+}
+
 const bankAccount = new BankAccount(1000);
 bankAccount.deposit(500);
 bankAccount.deposit(200);
